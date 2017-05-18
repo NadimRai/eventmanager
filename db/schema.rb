8527e30f5b249c423c252e5eda920bbf7abbbcf0
NadimRai <rai_nadim@yahoo.com>
2017-05-18 16:58:08 -0700

    used annotate gem to display category table in category.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170518233111) do
+ActiveRecord::Schema.define(version: 20170518234022) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
