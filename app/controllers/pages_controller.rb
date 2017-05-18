b703da43cd5221484ded774504fa95dd3c662ae8
NadimRai <rai_nadim@yahoo.com>
2017-05-18 10:30:04 -0700

    created partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+	#layout 'admin', {only: [:index]}
 	def index
+		@link = {content: 'our catalog', url: '/catalog'}
+	end
+
+	def about
+		@link = {content: 'our promo', url: '/promo'}
 	end
 end
