8df56eff145e9cae80e7abd838d230024107bb43
NadimRai <rai_nadim@yahoo.com>
2017-05-19 00:44:54 -0700

    validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,5 @@ class User < ApplicationRecord
   	has_many :events, through: :enrollments
 
   	validates :name, presence: true, length: {minimum: 2}
-  	validates :email, presence: true, uniqueness: true
+  	validates :email, presence: true, uniqueness: true, email: true
 end
