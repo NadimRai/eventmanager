83e7974157590c32b23cf7517caf053de83094ee
NadimRai <rai_nadim@yahoo.com>
2017-05-19 00:39:13 -0700

    added user model validation

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
+#
+
+class User < ApplicationRecord
+	has_many :enrollments
+  	has_many :events, through: :enrollments
+
+  	validates :name, presence: true, length: {minimum: 2}
+  	validates :email, presence: true, uniqueness: true
+end
