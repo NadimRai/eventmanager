bc6cfed2b4720b4c4076d93064a59f2796149a12
NadimRai <rai_nadim@yahoo.com>
2017-05-19 17:05:43 -0700

    created custom route for page and route for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/about', to: 'pages#about'
+
+  resources :categories
+  
+  resources :pages do 
+  	collection { get 'about'}
+  end
+  
   root 'pages#index'
 end
