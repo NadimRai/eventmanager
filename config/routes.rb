8ada3ba80a9e44cda47110cf1d0d78f078b1cd0c
NadimRai <rai_nadim@yahoo.com>
2017-05-20 01:41:58 -0700

    ability to create edit delete eventinside category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :categories
+  resources :categories do 
+  	resources :events, except: [:index]
+  end
   
   resources :pages do 
   	collection { get 'about'}
